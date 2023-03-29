2068280a3889ebd949d41fd8abf21c9261c314e4
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2023-03-29 19:37:21 +0200

    .

--- altery.sql
+++ altery.sql
@@ -67,3 +67,95 @@ FROM nazev_tabulky
 WHERE vyhledavaci_podminka
 GROUP BY seskupeni_podle_sloupce
 HAVING podminka_seskupeni;
+
+
+-- spravne having
+
+CREATE TABLE IF NOT EXISTS detail_objednavky(
+ id_objednavky int not null,
+ id_zbozi int not null,
+ pocet_kusu int not null,
+ cena_za_kus int not null,
+ primary key (id_objednavky, id_zbozi)
+);
+
+
+INSERT INTO detail_objednavky VALUES (1,1,15,159),(1,8,2,199),(2,5,3,1959),(2,1,4,2499),(3,24,1,99);
+
+
+
+SELECT id_objednavky,
+    SUM(pocet_kusu) AS kusy,
+    SUM(pocet_kusu*cena_za_kus) AS cena_celkem
+FROM detail_objednavky
+GROUP BY id_objednavky;
+
+
+
+SELECT id_objednavky,
+    SUM(pocet_kusu) AS kusy,
+    SUM(pocet_kusu*cena_za_kus) AS cena_celkem
+FROM detail_objednavky
+GROUP BY id_objednavky
+HAVING cena_celkem > 100;
+
+
+
+
+SELECT id_objednavky,
+    SUM(pocet_kusu) AS kusy,
+    SUM(pocet_kusu*cena_za_kus) AS cena_celkem
+FROM detail_objednavky
+GROUP BY id_objednavky
+HAVING cena_celkem > 100 AND kusy > 10;
+
+
+-- spravne having a where
+
+CREATE TABLE IF NOT EXISTS zakaznik(
+    id_zakaznika integer not null,
+    jmeno varchar(50) not null,
+    prijmeni varchar(50) not null,
+    vek int not null,
+    mesto varchar(50) not null,
+    primary key(id_zakaznika autoincrement)
+);
+
+
+INSERT INTO zakaznik
+VALUES
+(null,'Matěj','Eliáš',20,'Ostrava'),(null,'Karel','Svoboda',21,'Ostrava'),
+(null,'Jiří','Novák',17,'Ostrava'),(null,'Petr','Novotný',45,'Praha'),
+(null,'Jan','Horák',14,'Praha'),(null,'Prokop','Buben',34,'Brno');
+
+SELECT mesto, COUNT(id_zakaznika)
+FROM zakaznik
+GROUP BY mesto;
+
+
+SELECT mesto, COUNT(id_zakaznika) AS pocet
+FROM zakaznik
+GROUP BY mesto
+HAVING pocet > 1;
+
+
+SELECT mesto, COUNT(id_zakaznika) AS pocet
+FROM zakaznik
+WHERE vek > 17
+GROUP BY mesto
+HAVING pocet > 1;
+
+
+
+-- triggery 
+
+CREATE TRIGGER nazev_triggeru {BEFORE,AFTER} {UPDATE,INSERT,DELETE}
+ON nazev_pridruzene_tabulky FOR EACH ROW
+BEGIN
+    telo_triggeru;
+END $
+
+
+DROP TRIGGER [IF EXISTS] nazev_triggeru;
+
+
